79339a360c51141c197b946e75ad02cdddb95202
zergar5 <artem_kozhanov@mail.ru>
2022-11-30 19:59:06 +0700

    Добавьте файлы проекта.

---
+++ Crypt_4/Aes.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Crypt_4;
+
+public class Aes
+{
+    private readonly AesCryptoServiceProvider _aes;
+
+    public Aes()
+    {
+        _aes = new AesCryptoServiceProvider
+        {
+            BlockSize = 128,
+            KeySize = 256,
+            Mode = CipherMode.CBC,
+            Padding = PaddingMode.PKCS7
+        };
+    }
+    public string Encode(string plainText)
+    {
+        var cryptoTransform = _aes.CreateEncryptor();
+
+        var encodedBytes =
+            cryptoTransform.TransformFinalBlock(Encoding.UTF8.GetBytes(plainText), 0, plainText.Length);
+
+        return Convert.ToBase64String(encodedBytes);
+    }
+
+    public string Decode(string cypherText)
+    {
+        var cryptoTransform = _aes.CreateDecryptor();
+
+        var encodedBytes = Convert.FromBase64String(cypherText);
+
+        var decodedBytes = cryptoTransform.TransformFinalBlock(encodedBytes, 0, encodedBytes.Length);
+
+        return Encoding.UTF8.GetString(decodedBytes);
+    }
+
+    public void CreateKey(string key)
+    {
+        var keyHash = SHA256.HashData(Encoding.UTF8.GetBytes(key));
+        _aes.Key = keyHash;
+    }
+
+    public string CreateIV()
+    {
+        Random rand = new(100);
+        var iv = new byte[_aes.IV.Length];
+        rand.NextBytes(iv);
+        _aes.IV = iv;
+        return Convert.ToBase64String(iv);
+    }
+
+    public void SetIV(string iv)
+    {
+        _aes.IV = Convert.FromBase64String(iv);
+    }
+}
---
+++ Crypt_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using Crypt_4;
+
+try
+{
+    Console.WriteLine("Choose action: ");
+    Console.WriteLine("1 - encode text");
+    Console.WriteLine("2 - decode text");
+    var choose = Console.ReadLine();
+
+    var aes = new Aes();
+    Console.Write("Input key: ");
+    var key = Console.ReadLine();
+    aes.CreateKey(key);
+
+    switch (choose)
+    {
+        case "1":
+            var iv = aes.CreateIV();
+            Console.WriteLine($"IV: {iv}");
+            Console.Write("Input text: ");
+            var plainText = Console.ReadLine();
+            var encodedText = aes.Encode(plainText);
+            Console.WriteLine($"Encoded text: {encodedText}");
+            break;
+        case "2":
+            Console.Write("Input IV: ");
+            iv = Console.ReadLine();
+            aes.SetIV(iv);
+            Console.Write("Input text: ");
+            encodedText = Console.ReadLine();
+            var decodedText = aes.Decode(encodedText);
+            Console.WriteLine($"Decoded text: {decodedText}");
+            break;
+    }
+}
+catch (Exception)
+{
+    Console.WriteLine("Incorrect input");
+    throw;
+}
